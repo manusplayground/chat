e9f57a5ec7e7527324524d90831ec887909ed9d4
Manu Mehrotra <manu.mehrotra@oracle.com>
2019-01-20 11:47:03 -0800

    setuping up express promise router and making express use view engine ejs and body parsers. Also, telling express about static directory

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ dependencies.resolve(() => {
         const server = http.createServer(app);
         server.listen('8888', () => {
             console.log("listening on port 8888");
-        })
+        });
+
+        configureExpress(app);
+
+        const router = require("express-promise-router")();
+
+        app.use(router);
+    }
+
+    function configureExpress(app) {
+        app.use(express.static("public"));
+        app.set("view-engine", "ejs");
+        app.set(bodyParser.json());
+        app.set(bodyParser.urlencoded({extended: true}));
     }
 })
