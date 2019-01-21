1f692ba23c553351e12f231997a8f13efd34f5b7
Manu Mehrotra <manu.mehrotra@oracle.com>
2019-01-20 20:29:17 -0800

    adding ecpress session, validator and mongoose related packages

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const http = require("http");
 const ejs = require("ejs");
+const session = require("express-session");
+const validator = require("express-validator");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo")(session);
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
 const container = require("./container");
 
 container.resolve(function(users) {
@@ -26,7 +32,16 @@ container.resolve(function(users) {
     function configureExpress(app) {
         app.use(express.static("public"));
         app.set("view engine", "ejs");
-        app.set(bodyParser.json());
-        app.set(bodyParser.urlencoded({extended: true}));
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(validator());
+        app.use(cookieParser());
+        app.use(session({
+            secret: "kimmiethepoodledumb",
+            resave: true,
+            saveInitialized: true,
+            store: new MongoStore({mongooseConnection: mongoose.connection})
+        }));
+        app.use(flash());
     }
 })
